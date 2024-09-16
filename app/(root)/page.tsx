60b998751b2425ac5e285c37425ff5f83bd5a8cc
iuliadombrovschi <iulia.dombrovschi@isa.utm.md>
2024-09-17 02:43:34 +0300

    envrironment setup(Admin)

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
+const SetUpPage = () => {
+    return (
+     <div className="p-4">
+     <UserButton afterSignOutUrl="/" />
+     </div>
+    );
+  }
+  export default SetUpPage;
